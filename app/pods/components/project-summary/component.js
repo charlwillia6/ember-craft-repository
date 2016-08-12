89023a8544a5e0891b8a592698c070916876370e
Charles Williams <cwilli34@nd.edu>
2016-08-12 09:37:27 -0400

    Added project-summary widget

---
+++ app/pods/components/project-summary/component.js
@@ -0,0 +1,23 @@
+// app/pods/components/project-summary/component.js
+import Ember from 'ember';
+import layout from './template';
+
+export default Ember.Component.extend({
+    layout,
+    tagName: 'project-summary',
+    classNames: ['project', 'summary'],
+    store: Ember.inject.service(),
+    session: Ember.inject.service(),
+    init() {
+        this._super(...arguments);
+        console.log(this.get('model.tags'));
+    },
+    actions: {
+        addATag() {
+            this.sendAction('addATag', ...arguments);
+        },
+        removeATag() {
+            this.sendAction('removeATag', ...arguments);
+        }
+    }
+});
