2a0427d3a3ee0b70d5d03a961476b6432f66c651
Charles Williams <cwilli34@nd.edu>
2016-08-16 18:17:13 -0400

    Trying to fix issues with theme and site support and lessCompiler

---
+++ app/pods/components/file-browser/component.js
@@ -0,0 +1,17 @@
+// app/pods/components/file-browser/component.js
+import FileBrowser from 'ember-osf/components/file-browser/component';
+import layout from './template';
+
+export default FileBrowser.extend({
+    layout,
+    tagName: ['file-browser'],
+    classNames: ['file', 'browser'],
+    actions: {
+        openNode() {
+            this.sendAction(...arguments);
+        },
+        openFile() {
+            this.sendAction(...arguments);
+        }
+    }
+});
