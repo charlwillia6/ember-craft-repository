601237697e2b9a323aa232aaf8e8cef600f25e5a
Charles Williams <cwilli34@nd.edu>
2016-09-13 11:56:20 -0400

    Various fixes
    Fixed contrib-manager, contrib-add components, and other features added throughout.  Added the gravatar link.

--- app/pods/components/file-browser-icon/component.js
+++ app/pods/components/file-browser-icon/component.js
@@ -1,9 +1,27 @@
 // app/pods/components/file-browser-icon/component.js
-import FileBrowserIcon from 'ember-osf/components/file-browser-icon/component';
+// import FileBrowserIcon from 'ember-osf/components/file-browser-icon/component';
+import Ember from 'ember';
 import layout from './template';
 
-export default FileBrowserIcon.extend({
+export default Ember.Component.extend({
     layout,
+    tagName: '',
+    iconName: Ember.computed('item', 'item.expanded', function() {
+        // TODO: More icons!
+        if (this.get('item.isNode')) {
+            // TODO node types
+            return 'cube';
+        }
+        if (this.get('item.isProvider')) {
+            // TODO provider-specific icons
+            return 'disk outline';
+        }
+        if (this.get('item.isFolder')) {
+            return 'folder outline';
+        }
+        // TODO file types
+        return 'file outline';
+    })
     // attributeBindings:['elementId:id'],
     // elementId: 'file-browser',
     // classNames: ['file', 'browser'],
