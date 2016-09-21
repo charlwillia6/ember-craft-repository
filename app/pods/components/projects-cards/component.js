911e4a7356d91133887b33b91df96ea03ad35591
Charles Williams <cwilli34@nd.edu>
2016-09-21 18:24:52 -0400

    End of night commit

--- app/pods/components/projects-cards/component.js
+++ app/pods/components/projects-cards/component.js
@@ -1,3 +1,70 @@
+// app/pods/components/projects-cards/component.js
 import Ember from 'ember';
+import layout from './template';
+import PaginatedComponentMixin from 'ember-craft-repository/mixins/paginated-component';
 
-export default Ember.Component.extend({});
+export default Ember.Component.extend(PaginatedComponentMixin, {
+    layout,
+    currentUser: Ember.inject.service(),
+    user: null,
+    attributeBindings:['elementId:id'],
+    elementId: 'projects-cards',
+    classNames: ['projects', 'cards'],
+    isLoading: true,
+    init() {
+        this._super(...arguments);
+
+        if (this.get('session.isAuthenticated')) {
+            this._setCurrentUser();
+        }
+    },
+    _setCurrentUser() {
+        this.get('currentUser').load().then(user => this.set('user', user));
+    },
+    onGetCurrentUser: Ember.observer('user', function() {
+        this.loadProfileList();
+    }),
+    loadProfileList: function() {
+        var user = this.get('user');
+
+        if(user) {
+            this.set('user', user);
+        } else {
+            user = this.get('store').findRecord('user', 'me');
+            this.set('user', user);
+        }
+
+        console.log(this.get('page'));
+        var routeParams = {
+            page: this.get('page'),
+            page_size: 3
+        };
+
+        var userParams = {
+            filter: {
+                contributors: this.get('user.id'),
+                public: false
+            }
+        };
+
+        this.queryForComponent('node', routeParams, userParams);
+    },
+    willUpdate: function() {
+        this.set('isLoading', false);
+        // console.log(this.get('isLoading', false));
+    },
+    actions: {
+        next: function() {
+            this.incrementProperty('page', 1);
+            this.loadProfileList();
+        },
+        previous: function() {
+            this.decrementProperty('page', 1);
+            this.loadProfileList();
+        },
+        goToPage: function(pageNumber) {
+            this.set('page', pageNumber);
+            this.loadProfileList();
+        }
+    }
+});
--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -4,9 +4,10 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 import PaginatedRouteMixin from  'ember-osf/mixins/paginated-route';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, PaginatedRouteMixin, {
-    store: Ember.inject.service(),
     session: Ember.inject.service(),
-
+    store: Ember.inject.service(),
+    routeParams: null,
+    userParams: null,
     model(routeParams) {
         let user = this.modelFor('application');
         var userParams = {
@@ -15,16 +16,15 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, PaginatedRouteMixin, 
                 public: ''
             }
         };
+        console.log(routeParams);
 
-        routeParams = {
-            page: 1,
-            page_size: 3
-        }
+        routeParams['page_size'] = 3;
+        userParams['filter']['public'] = false;
+        this.set('routeParams', routeParams);
 
         if(user) {
             userParams['filter']['contributors'] = user.id;
-            userParams['filter']['public'] = false;
-            this.set('routeParams', routeParams);
+            this.set('userParams', userParams);
 
             return this.queryForPage('node', routeParams, userParams);
         } else {
@@ -32,27 +32,30 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, PaginatedRouteMixin, 
 
             return this.get('store').findRecord('user', 'me').then(function(user) {
                 userParams['filter']['contributors'] = user.id;
+                this.set('userParams', userParams);
+
                 return self.queryForPage('node', routeParams, userParams);
             });
         }
     },
-    setupController: function(controller, model) {
-        var publicParams = {
-            filter: {
-                contributors: '',
-                public: ''
-            }
-        };
-
+    setupController: function (controller, model) {
+        this._super(controller, model);
+        let user = this.modelFor('application');
+        let publicParams = this.get('userParams');
         let routeParams = this.get('routeParams');
 
-        routeParams = {
-            page: 1,
-            page_size: 6
+        if (user) {
+            controller.set('currentUser', user);
+        } else {
+            this.get('store').findRecord('user', 'me').then(function (user) {
+                controller.set('currentUser', user);
+            });
         }
-        console.log(this);
+
+        console.log('routeParams', this.get('routeParams'));
+
+        routeParams['page_size'] = 6;
         publicParams['filter']['public'] = true;
-        publicParams['filter']['contributors'] = this.get('user.id');
 
         controller.set('publicProjects', this.queryForPage('node', routeParams, publicParams));
         controller.set('model', model);
@@ -63,20 +66,3 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, PaginatedRouteMixin, 
         }
     }
 });
-
-// export default Ember.Route.extend(AuthenticatedRouteMixin, {
-//     session: Ember.inject.service(),
-//     store: Ember.inject.service(),
-//     setupController: function (controller) {
-//         this._super(controller);
-//         let user = this.modelFor('application');
-//
-//         if (user) {
-//             controller.set('currentUser', user);
-//         } else {
-//             this.get('store').findRecord('user', 'me').then(function (user) {
-//                 controller.set('currentUser', user);
-//             });
-//         }
-//     }
-// });
