fc0617a0d5fe1de6e578ccfea9920fd5416f9a56
Adam Brinckman <abrinckm@gmail.com>
2016-10-20 09:48:48 -0400

    Add mixin for utils

---
+++ app/mixins/utils-mixin.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+import RSVP from 'rsvp';
+import _ from 'lodash/lodash';
+
+export default Ember.Mixin.create({
+    ///////////////////////////////////////////////////////////////
+    collectionPluckAttributes: function(collection, attributes) {
+        return _.chain(collection)
+            .map(function(object) {
+                var hash = _.reduce(attributes, function(acc, _attribute) {
+                    _.set(acc, _attribute,  _.get(object, _attribute));
+                    return acc;
+                }, {});
+            })
+            .value();
+    },
+    ///////////////////////////////////////////////////////////////
+    DeferredPromise: function() {
+        let resolve = null;
+        let reject = null;
+
+        let promise = new RSVP.Promise((_resolve, _reject) => {
+            resolve = _resolve;
+            reject = _reject;
+        });
+
+        return {
+            resolve: resolve,
+            reject: reject,
+            promise: promise
+        };
+    }
+});
